fd1beda0a45eda34914c49fd40a59ad50f6f876b
Triforcey <superlama01@gmail.com>
2018-04-28 14:57:55 -0600

    added build script

---
+++ build.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var browserify = require('browserify');
+var path = require('path');
+if (!fs.existsSync('public/scripts')) {
+  fs.mkdirSync('public/scripts');
+}
+function compileScripts(dir) {
+  if (!fs.existsSync(path.join('public', dir))) {
+    fs.mkdirSync(path.join('public', dir));
+  }
+  fs.readdir(dir, function (err, files) {
+    if (err) throw err;
+    files.forEach(file => {
+      file = path.join(dir, file);
+      var stats = fs.statSync(file);
+      if (stats.isDirectory()) {
+        compileScripts(file);
+        return;
+      }
+      var b = browserify();
+      b.add(file);
+      var out = fs.createWriteStream(path.join('public', file));
+      b.bundle().pipe(out);
+    });
+  });
+}
+compileScripts('scripts');
