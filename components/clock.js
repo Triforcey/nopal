4f260e9c5b05437c11072fd872d59b474e8043c3
Triforcey <superlama01@gmail.com>
2018-04-29 20:37:11 -0600

    working on JSX on the server side

---
+++ components/clock.js
@@ -0,0 +1,8 @@
+var React = require('react');
+class Clock extends React.Component {
+  render() {
+    return (
+      <p>Hello world!</p>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+var express = require('express');
+var app = express();
+var mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+var server = require('http').createServer(app);
+
+var React = require('react');
+var reactDOM = require('react-dom');
+console.log(reactDOM.renderToString);
+var Clock = require('./components/clock.js');
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+server.listen(port, function () {
+  console.log(`Listening on port ${port}!`);
+});
