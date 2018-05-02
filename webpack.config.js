2c94b08ac9feafa6e94c503f221267c60c74e21b
Triforcey <superlama01@gmail.com>
2018-05-02 12:51:51 -0600

    satisfying my OCD

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': '\'process.env.NODE_ENV\''
     })
   ]
 };
