4b39caa06ad87aa67d168160ce6120f575c76dae
Triforcey <judsonwebdesign@gmail.com>
2018-05-05 19:22:15 -0600

    removed old code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,4 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-/*var Clock = require('../components/clock.js');
-clock.innerHTML = '';
-ReactDOM.render(<Clock />, clock);
-*/
 var Router = require('../components/router.js');
 ReactDOM.hydrate(<Router />, router);
