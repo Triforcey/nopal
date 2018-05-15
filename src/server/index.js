8bd92d5ac015e783ff5ef18efcd45428b0317373
Triforcey <judsonwebdesign@gmail.com>
2018-05-15 16:05:45 -0600

    removed obsolete code

--- src/server/index.js
+++ src/server/index.js
@@ -89,13 +89,9 @@ db.connect({
     });
   });
 
-  app.use((req, res, next) => {
-    res.status(404).send('404');
-  });
-
   app.use((err, req, res, next) => {
-    console.error(err);
     res.status(500).send('500');
+    console.error(err);
   });
 
   server.listen(port, function () {
